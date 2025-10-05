36dbcbf89d0a712f9351d9f1da80fe2b976414d3
luckyroma31-ctrl <luckyroma31@gmail.com>
2025-10-05 04:07:19 +0300

    Update StarWarsRashad.java

--- src/main/java/com/starwarsrashad/StarWarsRashad.java
+++ src/main/java/com/starwarsrashad/StarWarsRashad.java
@@ -1,1 +1,17 @@
+package com.starwarsrashad;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = StarWarsRashad.MODID, name = StarWarsRashad.NAME, version = StarWarsRashad.VERSION)
+public class StarWarsRashad {
+    public static final String MODID = "starwarsrashad";
+    public static final String NAME = "StarWars Rashad";
+    public static final String VERSION = "1.0";
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        System.out.println(NAME + " загружен!");
+    }
+}
 
